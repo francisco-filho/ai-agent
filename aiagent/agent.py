ee0608c05043c6e0621b731eb39f3b076f829f3a
Francisco Filho <francico@capimgrosso.com>
2025-01-14 15:20:43 -0300

    [wip] - agent with multi steps

--- aiagent/agent.py
+++ aiagent/agent.py
@@ -22,7 +22,7 @@ class Tool(BaseModel):
 class PythonCalculator(Tool):
     """
     calcular:
-    Executa um cáculo e retorna um número usando Python e garante que números flutuantes são usados quando necessários.
+    Executa um cáculo e retorna um número usando Python e garante que números flutuantes são usados quando necessários. O input para esta ação deve ser uma expressão python válida
     ex: calcular: 4 * 7 / 3
     ex: calcular aceita uma formula númerica
     """
@@ -63,7 +63,8 @@ class WikipediaTool(Tool):
 
 
     def run(self, q):
-        response = httpx.get("https://pt.wikipedia.org/w/api.php", params={
+        logging.info(f"wikipedia:params:{q}")
+        response = httpx.get("https://en.wikipedia.org/w/api.php", params={
             "action": "query",
             "list": "search",
             "srsearch": q,
@@ -76,6 +77,18 @@ class WikipediaTool(Tool):
 
     def __call__(self, q):
         return self.run(q)
+
+class ChatHistory:
+    messages: list[dict] = []
+
+    def __init__(self, system_prompt=""):
+        self.messages.append({"role": "system", "content": system_prompt})
+
+    def append(self, message: str):
+        self.messages.append({"role": "user", "content": message})
+
+    def response(self, message: str):
+        self.messages.append({"role": "assistant", "content": message})
 
 class Chatbot(BaseModel):
     system: str | None = None 
@@ -95,8 +108,9 @@ class ChatbotOllama(Chatbot):
         if system:
             self.messages.append({"role": "system", "content": self.system})
 
-    def execute(self):
-        response = ollama.chat(model= "gemma2:9b", messages=self.messages)
+    def execute(self, chat_history: ChatHistory):
+        #response = ollama.chat(model= "gemma2:9b", messages=chat_history.messages)
+        response = ollama.chat(model= "llama3.1", messages=chat_history.messages)
         completion = response['message']['content']
         return completion
 
@@ -119,10 +133,9 @@ calculator = PythonCalculator()
 wikipedia = WikipediaTool()
 
 prompt=f"""
-Você fala português e funciona em um loop de Pensamento, Ação, PAUSA e Observaçao.
 Quando uma pergunta for feita você vai:
-Usar Pensamento para descrever o que você acha que devem ser as ações a serem tomadas.
-As ferramentas dispníveis para ações são as seguintes:
+Usar Pensamento: para descrever o que você acha que devem ser as ações a serem tomadas.
+As ferramentas disponíveis para ações são as seguintes:
 
 Ações disponíveis:
 ---
@@ -137,11 +150,29 @@ Responda com o nome da Ação disponível que deve ser executada e então você 
 Observação será o resultado de executar uma Ação
 
 Você será chamado novamente com a observação
-No final do loop você exibe a resposta.
-
 
 Exemplo de uma sessão:
-Pergunta: Qual a capital da França? 
+Pergunta: Qual a capital da França e da Alemanha? 
+Pensamento: Eu devo pesquisar conte
+Ação: wikipedia: França
+Ação: wikipedia: Alemanha
+
+""";
+
+prompt2 = """
+Tente extrair a resposta final da observação recebida.
+Se você conseguir gerar a resposta final responder da seguinte forma:
+
+Resposta Final: {observação}
+
+Se não for a resposta final, você deve voltar para a fase de 'Pensamento'.
+"""
+
+
+p3="""No final do loop você exibe a resposta.
+
+Exemplo de uma sessão:
+Pergunta: Qual a capital da França e da Alemanha? 
 Pensamento: Eu devo pesquisar conte
 Ação: wikipedia: França
 PAUSA
@@ -159,7 +190,9 @@ action_re = re.compile(r"^Ação: (\w+): (.*)")
 
 def chat(question):
     #chat = ChatbotOpenAI(system="Você responde as questões da maneira mais suscinta possível, sem explicações")
-    chat = ChatbotOllama(system="Você responde as questões da maneira mais suscinta possível, sem explicações")
+    system="Você responde as questões da maneira mais suscinta possível, sem explicações"
+    hst = ChatHistory(system)
+    chat = ChatbotOllama(hst)
     return chat(question)
 
 
@@ -184,26 +217,42 @@ def query(question, max_turns=5):
     i = 0
     #bot = ChatbotOpenAI(prompt)
     bot = ChatbotOllama(prompt)
+    hst = ChatHistory(prompt)
+
     next_prompt = question
     print(f"Respondendo a questão: {question}")
     while i < max_turns:
         i += 1
-        result = bot(next_prompt)
+        print("Iteration: ", i)
+        hst.append(next_prompt)
+        result = bot.execute(hst)
+        hst.response(result)
+
+        if "Resposta Final" in result:
+            logging.info(result)
+            return
         
-        print(f"-------------------\n{result}\n-------------------\n")
-        if "Resposta:" in result:
-            return result
-        logging.info(result)
+        logging.info(f"Pensamento: {result}{'|fim_pensamento|'}")
+
         actions = [action_re.match(a) for a in result.split("\n") if action_re.match(a)]
         logging.info(f"actions: {actions} --> {result}")
         if actions:
-            action, input = actions[0].groups()
-            if action not in acoes:
-                raise Exception(f"Ação não encontrada: {action} {input}")
-            logging.info(f"Ação: {action}:{input} - Running")
-            obs = acoes[action](input)
-            logging.info(f"Observação: {obs}")
-            next_prompt = f"Observação: {obs}"
+            obs = ""
+            for a in actions:
+                action, input = a.groups()
+                if action not in acoes:
+                    logging.error(f"Ação não encontrada: {action} {input}")
+                    raise Exception(f"Ação não encontrada: {action} {input}")
+                logging.info(f"Ação: {action}:{input} - Running")
+                one_obs= acoes[action](input)
+                logging.info(f"Observação: {one_obs}")
+                obs = f"{obs}\n{one_obs}"
+            hst.append(f"Observação: {obs}\n\n{prompt2}")
+            next_prompt = bot.execute(hst) 
+            if "Resposta Final" in next_prompt:
+                logging.info(next_prompt)
+                return
+            logging.warn(f"Next prompt: {next_prompt}")
         else:
             return result
 
